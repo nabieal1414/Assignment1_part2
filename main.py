d379870db8906bbadb1d18953585df0b7bce146f
nabieal414 <nabieal14@gmail.com>
2022-08-30 14:10:48 -0400

    initial commit

--- main.py
+++ main.py
@@ -1,43 +1,16 @@
-#!/usr/bin/env python
-# _*_ coding: utf-8 _*_
+class Book:
+    author = ""
+    title = ""
+    def __init__(self, author, title):
+        self.author = author
+        self.title = title
 
-#week1 assignment1
-
-def listDivide (numbers, divide = 2):
-    """Functions and Exceptions: divide items in a list"""
-    """
-    Args:
-        numbers(list): list of int
-        divide=2 (int): number to divide by
-    Returns:
-        int : the number of elements in the list divisible by divisor.
-    Examples:
-         listDivide ([1,2,3,4,5])
-         2
-    """
-    divby2 = 0
-    for item in numbers:
-        if item % divide == 0:
-            divby2 += 1
-    return divby2
-
-class ListDivideException(Exception):
-    """Tow lines of class code"""
-    pass
-
-def testListDivide():
-    """To test and find out about exceptions"""
-
-    if listDivide ([1,2,3,4,5]) != 2:
-        raise ListDivideException
-    elif listDivide ([2,4,6,8,10]) !=5:
-        raise ListDivideException
-    elif listDivide ([30,54,63,98,100], 10) != 2:
-        raise ListDivideException
-    elif listDivide ([]) != 0:
-        raise ListDivideException
-    elif listDivide ([1,2,3,4,5], 1) != 5:
-        raise ListDivideException
+    def display(self):
+        print(f"{self.title}, written by {self.author}")
 
 if __name__ == "__main__":
-    testListDivide()
+    p1 = Book("John Steinbeck", "Of Mice and Men")
+    p2 = Book("Harper Lee", "To Kill a Mockingbird")
+
+    p1.display()
+    p2.display()
